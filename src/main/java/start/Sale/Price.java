ebf53bb9354850fba9e940f79697c5c4ab90ebba
Jakub <staw10@tlen.pl>
2016-07-07 22:20:48 +0200

    Pierwsze wrzucenie plików

---
+++ src/main/java/start/Sale/Price.java
@@ -0,0 +1,12 @@
+package start.Sale;
+
+/**
+ * Created by Michał on 2015-04-09.
+ */
+public class Price {
+    public double Price()
+    {
+        double price=0.0;
+        return price;
+    }
+}
